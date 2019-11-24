9d96dc073868e09f1dde2a851722ba97614880cc
Akiya <autumnsky64@gmail.com>
2019-11-24 17:26:59 +0900

    WIP 同期処理のため関数に分割

--- gilp.js
+++ gilp.js
@@ -2,31 +2,38 @@ function replaceGalleryImageHref( element, block, attributes ) {
 	if( block.name === 'core/gallery'
 		&& attributes.linkTo === 'attachment'){
 
-		// iterate type li className blocks-gallery-item
-		for( item of element.props.children[0].props.children ){
-
-			var imageId =item.props.children.props.children[0].props.children.props['data-id'];
-			var postUrl;
-			wp.apiFetch( { path: '/wp/v2/media/' + imageId } )
-				.then( function ( media ){
-					wp.apiFetch( { path: '/wp/v2/posts/' + media['post'] } )
-						.then( function ( post ){
-							postUrl = post.link;
-							console.log( postUrl );
-						})
-					});
-
-			item.props.children.props.children[0].props['href'] = postUrl;
-		}
-
-		return element;
+		return replaceList( element )
 
 	} else {
 
 		return element;
 
 	}
+}
 
+// iterate type li className blocks-gallery-item
+async function replaceList( element ){
+
+	for( item of element.props.children[0].props.children ){
+
+		var imageId =item.props.children.props.children[0].props.children.props['data-id'];
+		item.props.children.props.children[0].props['href'] = fetchPostUrl( imageId );
+
+	}
+
+	return element;
+
+}
+
+function fetchPostUrl( imageId ){
+	wp.apiFetch( { path: '/wp/v2/media/' + imageId } )
+		.then( function ( media ){
+			wp.apiFetch( { path: '/wp/v2/posts/' + media['post'] } )
+				.then( function ( post ){
+					console.log( post.link );
+					return post.link;
+				})
+			});
 }
 
 wp.hooks.addFilter(

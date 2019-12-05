f396f29107f4a804959d0ea864a90f0eb95893b9
Akiya <autumnsky64@gmail.com>
2019-12-05 15:04:07 +0900

    パーマリンクはattachmentまでが常に親の投稿URLではないので正規表現でのフィルター追加

--- gilp.js
+++ gilp.js
@@ -1,15 +1,16 @@
 function replaceGalleryImageHref( element, block, attributes ) {
 
 	if( block.name === 'core/gallery'
-		&& attributes.linkTo === 'attachment' ){
+		&& attributes.linkTo === 'attachment'
+	    && element.props.children.length > 0 ){
 
 		// iterate type li className blocks-gallery-item
 		for( item of element.props.children[0].props.children ){
 			var dataLink = item.props.children.props.children[0].props.children.props['data-link'];
-			var postUrl =dataLink.split('attachment')[0];
+			var re = /(http.+?\/\/.+?\/[0-9]+?\/[0-9]+?\/.+?\/).*/i;
+			var postUrl =dataLink.replace(re, '$1');
 
 			item.props.children.props.children[0].props['href'] = postUrl;
-
 		}
 	}
 
